c05e219c943c69eda1191b1d1b4f92d1ea5e7aab
Bryan Long <BryanLong1@gmail.com>
2018-03-23 12:59:42 -0600

    Fetch to API working

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
+import 'expo'
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
 import RootNavigation from './navigation/RootNavigation';
-
 export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
--- components/ScanResults.js
+++ components/ScanResults.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Text, View, StyleSheet, Alert } from "react-native";
 import { Constants, BarCodeScanner, Permissions } from "expo";
 
+// Compare Scan Results against Foods to Avoid and if there is a food that matches, change the style when the ingredient prints
+
 
 export default class ScanResults extends Component {
     render (){
