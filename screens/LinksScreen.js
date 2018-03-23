50ec9924ebc1ea7a32004555a464c5107e2f4740
Bryan Long <BryanLong1@gmail.com>
2018-03-23 14:54:58 -0600

    Added Icon image to header

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, Image, StyleSheet } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import BarcodeScan from '../components/BarcodeScanner'
 
@@ -10,8 +10,7 @@ export default class LinksScreen extends React.Component {
 
   render() {
     return <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
+        <Image source={require("../assets/images/CanIEatThatLogoMedium.png")} />
         <BarcodeScan />
       </ScrollView>;
   }
