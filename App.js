761cb5819f82e9f43306005f715840c3dfbb972a
Bryan Long <BryanLong1@gmail.com>
2018-03-27 16:29:37 -0600

    Changed Font

--- App.js
+++ App.js
@@ -1,28 +1,22 @@
-import 'expo'
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font } from 'expo';
-import { Ionicons } from '@expo/vector-icons';
-import RootNavigation from './navigation/RootNavigation';
+import "expo";
+import React from "react";
+import { Platform, StatusBar, StyleSheet, View } from "react-native";
+import { AppLoading, Asset, Font } from "expo";
+import { Ionicons } from "@expo/vector-icons";
+import RootNavigation from "./navigation/RootNavigation";
 export default class App extends React.Component {
   state = {
-    isLoadingComplete: false,
+    isLoadingComplete: false
   };
 
   render() {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
+      return <AppLoading startAsync={this._loadResourcesAsync} onError={this._handleLoadingError} onFinish={this._handleFinishLoading} />;
     } else {
       return (
         <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          {Platform.OS === 'android' && <View style={styles.statusBarUnderlay} />}
+          {Platform.OS === "ios" && <StatusBar barStyle="default" />}
+          {Platform.OS === "android" && <View style={styles.statusBarUnderlay} />}
           <RootNavigation />
         </View>
       );
@@ -33,12 +27,8 @@ export default class App extends React.Component {
     return Promise.all([
       Asset.loadAsync([require("./assets/images/robot-dev.png"), require("./assets/images/robot-prod.png")]),
       Font.loadAsync({
-        // This is the font that we are using for our tab bar
         ...Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        "space-mono": require("./assets/fonts/SpaceMono-Regular.ttf"),
-        "RammettoOne-Regular": require("./assets/fonts/RammettoOne-Regular.ttf")
+        "Dosis-Regular": require("./assets/fonts/Dosis-Regular.ttf")
       })
     ]);
   };
@@ -57,10 +47,10 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
   statusBarUnderlay: {
     height: 24,
-    backgroundColor: 'rgba(0,0,0,0.2)',
-  },
+    backgroundColor: "rgba(0,0,0,0.2)"
+  }
 });
