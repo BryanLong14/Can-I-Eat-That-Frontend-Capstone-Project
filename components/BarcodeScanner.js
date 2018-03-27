8cd827aa1be019be5f842e3e831bc2f5f2c55c49
Bryan Long <BryanLong1@gmail.com>
2018-03-27 10:21:53 -0600

    Version with Test Data disasbled

--- components/BarcodeScanner.js
+++ components/BarcodeScanner.js
@@ -15,21 +15,21 @@ export default class BarcodeScan extends Component {
     this._requestCameraPermission();
   }
 
-  lookupUPC = () => {
-    this.setState({
-      results: APIresults
-    });
-  };
-
-  // lookupUPC = UPC => {
-  //   fetch(`https://can-i-eat-that-api.herokuapp.com/api/scan/${UPC}`)
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       this.setState({
-  //         results: data
-  //       });
-  //     });
+  // lookupUPC = () => {
+  //   this.setState({
+  //     results: APIresults
+  //   });
   // };
+
+  lookupUPC = UPC => {
+    fetch(`https://can-i-eat-that-api.herokuapp.com/api/scan/${UPC}`)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          results: data
+        });
+      });
+  };
 
   _requestCameraPermission = async () => {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
@@ -60,7 +60,7 @@ export default class BarcodeScan extends Component {
         ) : this.state.results === "" ? (
           <View style={styles.barcodeContainer}>
             <Text style={styles.H2}>Scan Your Barcode Below</Text>
-            <Button title="Dummy Data" onPress={() => this.lookupUPC()} style={styles.button} />
+            {/* <Button title="Dummy Data" onPress={() => this.lookupUPC()} style={styles.button} /> */}
             <BarCodeScanner info={this.state} torchMode="on" onBarCodeRead={this._handleBarCodeRead} style={{ height: 200, width: 200 }} />
           </View>
         ) : (
